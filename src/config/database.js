b9e6ffee854138f4cfabbedfdab06d13a45b89a2
Shedrack Massaburi <didasshedrack@gmail.com>
2025-03-28 22:17:45 +0300

    changed database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,11 @@
-const mysql = require('mysql');
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+dotenv.config();
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the MySQL database');
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
 });
